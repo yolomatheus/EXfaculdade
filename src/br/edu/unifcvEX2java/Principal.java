78cac17aa2a569cb08ac68c1cf0ae25c5accba56
unknown <matheus.psq@hotmail.com>
2019-10-23 20:50:09 -0300

    H removido

--- src/br/edu/unifcvEX2java/Principal.java
+++ src/br/edu/unifcvEX2java/Principal.java
@@ -47,7 +47,7 @@ public class Principal {
 		Operario O = new Operario();
 		
 		O.setNome("Luiz");
-		O.setEndereço("Joaquim hBarbosa");
+		O.setEndereço("Joaquim Barbosa");
 		O.setTelefone("5274-1875");
 		O.setCodigoSetor(3);
 		//O.CalcularSalario();
